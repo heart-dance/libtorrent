83e793b25184a2d1ca73ff495037b5f51ef0dc30
gsxhnd <gsxhnd@gmail.com>
2022-03-31 01:05:29 +0800

    update

--- bencode.go
+++ bencode.go
@@ -1,2 +1,28 @@
 package libtorrent
 
+import (
+	"io"
+
+	"github.com/jackpal/bencode-go"
+)
+
+type bencodeInfo struct {
+	Pieces       string `bencode:"pieces"`
+	PiecesLength int    `bencode:"piece length"`
+	Length       int    `bencode:"Length"`
+	Name         string `bencode:"name"`
+}
+
+type bencodeTorrent struct {
+	Announce string      `bencode:"announce"`
+	Info     bencodeInfo `bencode:"info"`
+}
+
+func Open(r io.Reader) (*bencodeTorrent, error) {
+	bto := &bencodeTorrent{}
+	err := bencode.Unmarshal(r, bto)
+	if err != nil {
+		return nil, err
+	}
+	return bto, nil
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/heart-dance/libtorrent
 
 go 1.17
+
+require github.com/jackpal/bencode-go v1.0.0 // indirect
